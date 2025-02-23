a10e0f979249f8ab0ae8ee7c981f4e72dd8932b3
amandacolacioRethink <amanda.colacio@rethink.dev>
2025-02-23 16:21:23 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Products page</h1>;<Button>Teste</Button>
+    </div>
+  );
+};
+
+export default ProductPage;
