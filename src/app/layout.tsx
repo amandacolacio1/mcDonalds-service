4f04ad6cc7140fa620fbf0ef284c09af6715ca91
amandacolacioRethink <amanda.colacio@rethink.dev>
2025-02-23 16:38:34 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
+
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -8,8 +9,8 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Mc Donalds",
-  description: "Bora finalizar esse projeto lindo!",
+  title: "MC Donalds",
+  description: "Sistema de atendimento do MC Donalds",
 };
 
 export default function RootLayout({
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 const ProductPage = () => {
   return (
     <div>
-      <h1 className="text-red-500">Products page</h1>;<Button>Teste</Button>
+      <h1 className="flex text-red-500">Products page</h1>;
+      <Button>Teste</Button>
     </div>
   );
 };
